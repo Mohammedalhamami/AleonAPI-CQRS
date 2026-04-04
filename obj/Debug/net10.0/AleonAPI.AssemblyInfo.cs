f7cad69beb4eb41ae6599e3977befc829d90a310
mohammed <mohammed.haider.alhamami1999@gmail.com>
2026-04-04 08:40:53 +0300

    first commit

--- obj/Debug/net10.0/AleonAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/AleonAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleonAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c1bc9bf98608350554d375176a6f3b3a9f1e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleonAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleonAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
